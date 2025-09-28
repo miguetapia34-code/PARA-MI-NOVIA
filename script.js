ac846154bd3ce1a6f40b63890cf0d8ec3d3b69df
miguetapia34-code <miguetapia34@gmail.com>
2025-09-28 00:43:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,28 +7,25 @@ document.addEventListener("click", (e) => {
         e.target.matches(".solapa-izquierda") ||
         e.target.matches(".corazon")) {
         envoltura.classList.toggle("abierto");
-        envoltura.classList.add("desactivar-sobre")
-
+      
+    } else if (e.target.matches(".sobre *")) {
         if (!carta.classList.contains("abierta")) {
-            setTimeout(() => {
-                carta.classList.add("mostrar-carta");
+            carta.classList.add("mostrar-carta");
 
-                setTimeout(() => {
-                    carta.classList.remove("mostrar-carta");
-                    carta.classList.add("abierta");
-                }, 500);
-            }, 1000);
-        }
-    } else if (e.target.matches(".envoltura-sobre *")) {
-        envoltura.classList.remove("abierto");
-        envoltura.classList.remove("desactivar-sobre")
-        if (carta.classList.contains("abierta")) {
+            setTimeout(() => {
+                carta.classList.remove("mostrar-carta");
+                carta.classList.add("abierta");
+            }, 500);
+            envoltura.classList.add("desactivar-sobre")
+        } else {
             carta.classList.add("cerrando-carta");
+            envoltura.classList.remove("desactivar-sobre");
 
             setTimeout(() => {
-                carta.classList.remove("cerrando-carta");
+                carta.classList.remove("cerrando-carta")
                 carta.classList.remove("abierta")
             }, 500);
         }
-    }
+
+    } 
 })
